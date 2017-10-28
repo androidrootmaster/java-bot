9032b0ef0794ef4bc2f6a98b47e9c9a18878f703
androidrootmaster <33175159+androidrootmaster@users.noreply.github.com>
2017-10-28 11:48:00 +0200

    Add files via upload

---
+++ assistant.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+class Bot
+{
+public static void main(String[]args)
+{
+String frage;
+Scanner scan = new Scanner(System.in);
+System.out.println("das ist eine Beta");
+System.out.println("_____________________________________________________________________________________________________________");
+System.out.println("|||          |||||||||  ||||||||||||||   |||||||||||||   ||||    |||||||||||    ||||||||||                                        ");
+System.out.println("|||          ||    |||  ||||      ||||   ||||      |||           ||||   ||||    ||||||||||                                        ");
+System.out.println("|||          ||    |||  ||||      ||||   ||||      |||   ||||    ||||   ||||    ||||                                              ");
+System.out.println("|||          ||    |||  ||||      ||||   ||||      |||   ||||    ||||   ||||    ||||      ||||||                                  ");
+System.out.println("|||          ||    |||  ||||||||||||||   ||||      |||   ||||    ||||   ||||    ||||       |||                                    ");
+System.out.println("|||          ||    |||  ||||      ||||   ||||      |||   ||||    ||||   ||||    ||||||||||||||      |||| ||||                     ");
+System.out.println("||||||||||   |||||||||  ||||      ||||   |||||||||||||   ||||    ||||   ||||    ||||||||||||||      |||| ||||                     ");
+System.out.println("-------------------------------------------------------------------------------------------------------------");
+System.out.print(" chatbot:ich Bin Bot der  (Hi oder ende):");   // ja ich habe ihm noch keinen gescheiten namen gegeben 
+System.out.println("");
+frage =scan.nextLine();
+if(frage.equals("Hi"))
+System.out.println("Guten Tag");
+else
+System.out.println("    _sie wollen schon gehen!!   ");
+System.out.print("  _Wie gehts ihnen     (gut oder Schlecht)(dumm):");// ja ich bin anfänger ist ziemlich unübersichtlich XD aber ich verstehe alles
+frage=scan.nextLine();
+if(frage.equals("gut"))
+System.out.println("    _Freut mich mir gehts auch gut  ");
+if(frage.equals("dumm"))
+System.out.println("    _guck dich mal selber an    ");
+if(frage.equals("schlecht"))
+System.out.println("    sagt_das tut mir leid"   );
+System.out.print("  _sind sie älter als 99!!    (ja oder nein):");
+frage=scan.nextLine();
+if(frage.equals("ja"))
+System.out.println("sie sind aber ziemlich alt  ");
+else
+System.out.println("    das ist gut XD  ");
+System.out.print("   was ist ihr lieblingstier   (katze oder Hund):");
+frage=scan.nextLine();
+if(frage.equals("katze"))
+System.out.println("ok cool mein lieblingstier ist der vogel");
+else
+System.out.println("ok cool mein lieblingstier ist der Affe");
+System.out.println("ich stelle dir jetzt ein paar Fragen(ja oder nein):");
+frage=scan.nextLine();
+if(frage.equals("ja"))
+System.out.println("=======ok gut gleich kommen die Fragen");
+else
+System.out.println("=======schade warum nicht");
+System.out.println("was ist kein Essen?");
+System.out.println("tomate");
+System.out.println("Auto");
+frage=scan.nextLine();
+if (frage.equals("Auto"))
+System.out.println("ja Stimmt 100% richtig");
+else
+System.out.println("falsch tomate ist essen");
+System.out.println("ok lassen wir das mit den fragen was willst du als nächstes(was kannst du oder nix):");
+frage =scan.nextLine();
+if (frage.equals("was kannst du"))
+System.out.println("Das was du nicht kannst");
+else
+System.out.println("ok dann ist es halt vorbei");
+System.out.print("lass uns chatten(frau oder mann):");
+frage=scan.nextLine();
+if (frage.equals("mann"))
+System.out.println("ok  der herr irgendwas ich bin heute ziemlich frech");
+if (frage.equals("frau"))
+System.out.println("na du schnecke");
+System.out.print("wie langweilig ist dir (viel oder wenig):");
+frage =scan.nextLine();
+if (frage.equals("viel"))
+System.out.println("ja das merkt mann");
+else
+System.out.println("das ist gut freut mich das ich dich unterhalten kann");
+System.out.println("----------------------------------------------------------------------------------");
+System.out.println("||||||||||||||||||||||||||    ||||||||||||||||||||||||   ||||||||||||||||||       ");                       
+System.out.println("||||||||||||||||||||||||||    |||||||            |||||   |||         ||||||       ");
+System.out.println("|||||||                       |||||||            |||||   |||         ||||||       ");
+System.out.println("|||||||                       |||||||            |||||   |||         ||||||       ");
+System.out.println("|||||||                       |||||||            |||||   |||         ||||||       ");
+System.out.println("|||||||                       |||||||            |||||   |||         ||||||       ");
+System.out.println("|||||||                       |||||||            |||||   |||         ||||||       ");
+System.out.println("||||||||||||||||||||||||||    |||||||            |||||   |||         ||||||       ");
+System.out.println("||||||||||||||||||||||||||    |||||||            |||||   |||         ||||||       ");
+System.out.println("|||||||                       |||||||            |||||   |||         ||||||       ");
+System.out.println("|||||||                       |||||||            |||||   |||         ||||||       ");
+System.out.println("|||||||                       |||||||            |||||   |||         ||||||       ");
+System.out.println("||||||||||||||||||||||||||    |||||||            |||||   |||         ||||||       ");
+System.out.println("||||||||||||||||||||||||||    |||||||            |||||   |||||||||||||||||||      ");
+System.out.println("----------------------------------------------------------------------------------");
+}
+}
+/** @author Walter */
+/** @version 1.0.0 beta*/
+
+
+
